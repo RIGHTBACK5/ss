33bee3a2e4c46aef03ab48469548524100309daf
hotsoycandy <wkdwnsghd08@naver.com>
2016-08-15 20:27:49 +0900

    제이쿼리 다 파일 다운받아서 연결함
    진짜임 레알 이제 인터넷 없어도 됨

--- JS/slide.js
+++ JS/slide.js
@@ -1,11 +1,12 @@
 $(function(){
-        $(".home").click(function(){
+    $(".home").click(function(){
         $("body").css("background","#5F5F5F");
         $("section").css("height","750px");
         $("section").css("width","1600px");
         $("article").css("display","none");
         $("#main_wrap").css("display","block");
         $("#b-img").css("opacity","0");
+        $("#ss_wrap").css("display","none");
     });
             
     $(".hangang").click(function(){
@@ -15,6 +16,7 @@ $(function(){
         $("#main_wrap").css("display","none");
         $("article").css("display","inline-block");
         $("#b-img").css("opacity","0");
+        $("#ss_wrap").css("display","none");
     });
     
     $(".ss").click(function(){
@@ -23,6 +25,7 @@ $(function(){
         $("article").css("display","none");
         $("#main_wrap").css("display","none");
         $("#b-img").css("opacity","100");
+        $("#ss_wrap").css("display","block");
     });
     $(".au").click(function(){
         $("body").css("background","#E1E18C");
@@ -31,86 +34,88 @@ $(function(){
         $("article").css("display","none");
         $("#main_wrap").css("display","none");
         $("#b-img").css("opacity","0");
+        $("#ss_wrap").css("display","none");
     });
-    
-    var current_m = 0;
-    var currentX = 0;
-    var currentY = 0;
-    var max_m = 0;
-    var maxX = 7;
-    var maxY = 3;
-    var container_m;
-    var container_h;
-
-    function init(){
-        container_m = $(".slide_m>ul");
-        container_h = $(".slide_h>ul");
-        
-        max_m = container_m.children().length;
-        
-        events();
-        setInterval(next_m, 5000);
-    }
-    function events(){
-        $(window).on("keydown",keydown);
-    }
-    
-    function next_m(e){
-        current_m++;
-        if(current_m > max_m-1) current_m = 0;
-        animate_m();
-    } 
-    
-    function prev(e){
-        currentX--;
-        if(currentX < 0) currentX = maxX-1;
-        animate_h(1);
-    }
-    function next(e){
-        currentX++;
-        if(currentX > maxX-1) currentX = 0;
-        animate_h(1);
-    }    
-    function up(e){
-        currentY--;
-        if(currentY < 0) currentY = maxY-1;
-        animate_h(2);
-    }
-    function down(e){
-        currentY++;
-        if(currentY > maxY-1) currentY = 0;
-        animate_h(2);
-    }
-    
-    function keydown(e){
-        if(e.which == 37){
-            prev();
-        }
-        else if(e.which == 39){
-            next();
-        }
-        else if(e.which == 38){
-            up();
-        }
-        else if(e.which == 40){
-            down();
-        }
-    }
-    
-    function animate_m(){
-        var moveX = current_m * 1129;
-        TweenMax.to(container_m, 0, {marginLeft : -moveX, ease:Expo.easeOut}); 
-    }
-    function animate_h(s){
-        var moveX = currentX * 1200;
-        var moveY = currentY * 750;
-        if(s==1){
-            TweenMax.to(container_h, 0, {marginLeft : -moveX, ease:Expo.easeOut});
-        }
-        else{
-            TweenMax.to(container_h, 0, {marginTop : -moveY, ease:Expo.easeOut});
-        }
-    }
-    
-    $(document).ready( init );
 });
+var current_m = 0;
+var currentX = 0;
+var currentY = 0;
+var max_m = 0;
+var maxX = 13;
+var maxY = 5;
+var container_m;
+var container_h;
+var s = $("#sBox_wrap > li");
+
+function init(){
+    container_m = $(".slide_m>ul");
+    container_h = $(".slide_h>ul");
+
+    max_m = container_m.children().length;
+
+    events();
+    setInterval(next_m, 5000);
+}
+function events(){
+    $(window).on("keydown",keydown);
+}
+
+function next_m(e){
+    current_m++;
+    if(current_m > max_m-1) current_m = 0;
+    animate_m();
+} 
+
+function prev(e){
+    currentX--;
+    if(currentX < 0) currentX = 0;
+    animate_h(1);
+}
+function next(e){
+    currentX++;
+    if(currentX > maxX-1) currentX = maxX-1;
+    animate_h(1);
+}    
+function up(e){
+    currentY--;
+    if(currentY < 0) currentY = 0;
+    animate_h(2);
+}
+function down(e){
+    currentY++;
+    if(currentY > maxY-1) currentY = maxY-1;
+    animate_h(2);
+}
+
+function keydown(e){
+    if(e.which == 37){
+        prev();
+    }
+    else if(e.which == 39){
+        next();
+    }
+    else if(e.which == 38){
+        up();
+    }
+    else if(e.which == 40){
+        down();
+    }
+}
+
+function animate_m(){
+    var moveX = current_m * 1129;
+    TweenMax.to(container_m, 0, {marginLeft : -moveX, ease:Expo.easeOut});
+    $("#sBox_wrap >li").removeClass("np");
+    $(s[current_m]).addClass("np");
+}
+function animate_h(s){
+    var moveX = currentX * 600;
+    var moveY = currentY * 375;
+    if(s==1){
+        TweenMax.to(container_h, 0, {marginLeft : -moveX, ease:Expo.easeOut});
+    }
+    else{
+        TweenMax.to(container_h, 0, {marginTop : -moveY, ease:Expo.easeOut});
+    }
+}
+$(document).ready( init );
